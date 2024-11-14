8ad977d7d3b9f9ef9bf7d122762a6407c9d1032f
Isabel Kleefeld <isabel.kleefeld@tu-berlin.de>
2024-11-14 14:36:25 +0100

    Update cleandata.R

--- woerterbuch/R/cleandata.R
+++ woerterbuch/R/cleandata.R
@@ -11,28 +11,36 @@
 #' @return Ein DataFrame mit den formatierten Ergebnissen.
 #' @export
 process_findings <- function(relevant_documents) {
-  # Ergebnisse in Datenrahmen-Format bringen
+  # Ergebnisse in Datenrahmen-Format bringen und mögliche Duplikate behandeln
   findings_df <- rrapply(relevant_documents, how = "melt")
-
-  # Gruppen für mehrfach Treffer pro Dokument identifizieren
   findings_df$group <- cumsum(findings_df$L1 == "doc_id")
 
-  # Dynamische Zuordnung der Spaltennamen
+  # Dynamische Spaltenbenennung und Auswahl
   findings_df <- findings_df %>%
     select(L1, L2, value, group) %>%
     mutate(L1 = ifelse(L2 == "document", L1, paste0(L1, "_", L2))) %>%
     select(-L2)
 
-  # Umwandlung in breites Format und Entpacken von Listen
+  # Umwandlung in breites Format
+  # Verwenden von values_fn, um Listenspalten zu vermeiden und Mehrfachwerte zu aggregieren
   results <- findings_df %>%
-    pivot_wider(names_from = "L1", values_from = "value") %>%
-    unnest(cols = c(any_of(names(findings_df)[grepl("^found_words_", names(findings_df))]), "document", "doc_id"))
+    pivot_wider(names_from = "L1", values_from = "value", values_fn = list(value = list)) %>%
+    mutate(across(starts_with("found_words"), ~ sapply(.x, function(x) paste0(unlist(x), collapse = ", "))))
 
-  # Zusammenfassen der Wörter pro Dokument (dynamisch alle gefundenen Gruppen zusammenführen)
-  results <- results %>%
-    group_by(doc_id, document) %>%
-    summarize(across(starts_with("found_words"), ~ paste0(.x, collapse = ", "))) %>%
-    ungroup()
+  # Dokument- und Wort-Spalten zusammenfassen
+  if ("document" %in% names(results)) {
+    results <- results %>%
+      unnest(cols = c(document, doc_id)) %>%
+      group_by(doc_id, document) %>%
+      summarize(across(starts_with("found_words"), ~ paste0(.x, collapse = ", "))) %>%
+      ungroup()
+  } else {
+    results <- results %>%
+      unnest(cols = doc_id) %>%
+      group_by(doc_id) %>%
+      summarize(across(starts_with("found_words"), ~ paste0(.x, collapse = ", "))) %>%
+      ungroup()
+  }
 
   return(results)
 }
