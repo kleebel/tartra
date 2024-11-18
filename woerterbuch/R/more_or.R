be9c8df106282922cdcff32ac3359fdbfd307573
Isabel Kleefeld <isabel.kleefeld@tu-berlin.de>
2024-11-18 15:48:29 +0100

    Update more_or.R

--- woerterbuch/R/more_or.R
+++ woerterbuch/R/more_or.R
@@ -11,7 +11,7 @@
 #' @export
 moreor_apply_search <- function(corpus, conditions) {
   # Erstelle einen Parallel-Cluster
-  cl <- parallel:makeCluster(detectCores())
+  cl <- doParallel:makeCluster(detectCores())
   registerDoParallel(cl)
 
   # Parallele Verarbeitung der Dokumente
